d08a6a4c946ea45a62c911794b8fc190306951d1
Mike Sasiela <m5455y@gmail.com>
2021-03-01 19:37:40 -0600

    fixed Stories page. made some styling adjustments

--- public/assets/js/adopt.js
+++ public/assets/js/adopt.js
@@ -1,36 +1,46 @@
-const changeAdoptedBtns = document.querySelectorAll('.change-adopted');
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+document.addEventListener('DOMContentLoaded', (event) => {
+  if (event) {
+    console.info('DOM loaded');
+  }
 
-// Set up the event listener for the create button
-if (changeAdoptedBtns) {
-  changeAdoptedBtns.forEach((button) => {
-    button.addEventListener('click', (e) => {
-      // Grabs the id of the element that goes by the name, "id"
-      const id = e.target.getAttribute('data-id');
-      const newAdopt = e.target.getAttribute('data-adopted');
+  const changeAdoptedBtns = document.querySelectorAll('.change-adopted');
 
-      const newAdoptionStatus = {
-        adopted: newAdopt,
-      };
+  // Set up the event listener for the create button
+  if (changeAdoptedBtns) {
+    changeAdoptedBtns.forEach((button) => {
+      button.addEventListener('click', (e) => {
 
-      fetch(`/api/dogs/${id}`, {
-        method: 'PUT',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
+        // Grabs the id of the element that goes by the name, "id"
+        const id = e.target.getAttribute('data-id');
+        const newAdopt = e.target.getAttribute('data-newAdoptionStatus');
 
-        // make sure to serialize the JSON body
-        body: JSON.stringify(newAdoptionStatus),
-      }).then((response) => {
+        const newAdoptionStatus = {
+          adopted: newAdopt,
+        };
+
+        console.log(newAdoptionStatus)
+
+        fetch(`/api/dogs/${id}`, {
+          method: 'PUT',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+
+          // make sure to serialize the JSON body
+          body: JSON.stringify(newAdoptionStatus),
+        }).then((response) => {
         // Check that the response is all good
         // Reload the page so the user can see the new quote
-        if (response.ok) {
-          console.log(`changed status to: ${newAdopt}`);
-          location.reload('/');
-        } else {
-          alert('something went wrong!');
-        }
+          if (response.ok) {
+            console.log(`changed status to: ${newAdopt}`);
+            location.reload('/');
+          } else {
+            alert('something went wrong!');
+          }
+        });
       });
     });
-  });
-}
+  }
+})
